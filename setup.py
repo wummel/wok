1043822e11ff58c37475e3fdfd1704acd37dcba4
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-03-27 19:14:27 +0100

    Install config data as extra file.

--- setup.py
+++ setup.py
@@ -1,12 +1,136 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+import os
+import sys
+from distutils.core import setup, Distribution
+from distutils.command.install_lib import install_lib
+from distutils import util
+from distutils.file_util import write_file
 
-from setuptools import setup
 
-from wok import version
+AppName = 'wok'
+AppVersion = '0.10'
+
+def normpath (path):
+    """Norm a path name to platform specific notation."""
+    return os.path.normpath(path)
+
+
+def cnormpath (path):
+    """Norm a path name to platform specific notation and make it absolute."""
+    path = normpath(path)
+    if os.name == 'nt':
+        # replace slashes with backslashes
+        path = path.replace("/", "\\")
+    if not os.path.isabs(path):
+        path = normpath(os.path.join(sys.prefix, path))
+    return path
+
+
+class MyInstallLib (install_lib, object):
+    """Custom library installation."""
+
+    def install (self):
+        """Install the generated config file."""
+        outs = super(MyInstallLib, self).install()
+        infile = self.create_conf_file()
+        outfile = os.path.join(self.install_dir, os.path.basename(infile))
+        self.copy_file(infile, outfile)
+        outs.append(outfile)
+        return outs
+
+    def create_conf_file (self):
+        """Create configuration file."""
+        cmd_obj = self.distribution.get_command_obj("install")
+        cmd_obj.ensure_finalized()
+        # we have to write a configuration file because we need the
+        # <install_data> directory (and other stuff like author, url, ...)
+        # all paths are made absolute by cnormpath()
+        data = []
+        for d in ['purelib', 'platlib', 'lib', 'headers', 'scripts', 'data']:
+            attr = 'install_%s' % d
+            if cmd_obj.root:
+                # cut off root path prefix
+                cutoff = len(cmd_obj.root)
+                # don't strip the path separator
+                if cmd_obj.root.endswith(os.sep):
+                    cutoff -= 1
+                val = getattr(cmd_obj, attr)[cutoff:]
+            else:
+                val = getattr(cmd_obj, attr)
+            if attr == 'install_data':
+                cdir = os.path.join(val, "share", "dosage")
+                data.append('config_dir = %r' % cnormpath(cdir))
+            elif attr == 'install_lib':
+                if cmd_obj.root:
+                    _drive, tail = os.path.splitdrive(val)
+                    if tail.startswith(os.sep):
+                        tail = tail[1:]
+                    self.install_lib = os.path.join(cmd_obj.root, tail)
+                else:
+                    self.install_lib = val
+            data.append("%s = %r" % (attr, cnormpath(val)))
+        self.distribution.create_conf_file(data, directory=self.install_lib)
+        return self.get_conf_output()
+
+    def get_conf_output (self):
+        """Get filename for distribution configuration file."""
+        return self.distribution.get_conf_filename(self.install_lib)
+
+    def get_outputs (self):
+        """Add the generated config file to the list of outputs."""
+        outs = super(MyInstallLib, self).get_outputs()
+        outs.append(self.get_conf_output())
+        return outs
+
+
+class MyDistribution (Distribution, object):
+    """Custom distribution class generating config file."""
+
+    def __init__ (self, attrs):
+        """Set console and windows scripts."""
+        super(MyDistribution, self).__init__(attrs)
+        self.console = ['dosage']
+
+    def run_commands (self):
+        """Generate config file and run commands."""
+        cwd = os.getcwd()
+        data = []
+        data.append('config_dir = %r' % os.path.join(cwd, "config"))
+        data.append("install_data = %r" % cwd)
+        data.append("install_scripts = %r" % cwd)
+        self.create_conf_file(data)
+        super(MyDistribution, self).run_commands()
+
+    def get_conf_filename (self, directory):
+        """Get name for config file."""
+        return os.path.join(directory, "_%s_configdata.py" % self.get_name())
+
+    def create_conf_file (self, data, directory=None):
+        """Create local config file from given data (list of lines) in
+        the directory (or current directory if not given)."""
+        data.insert(0, "# this file is automatically created by setup.py")
+        data.insert(0, "# -*- coding: iso-8859-1 -*-")
+        if directory is None:
+            directory = os.getcwd()
+        filename = self.get_conf_filename(directory)
+        # add metadata
+        metanames = ("name", "version", "author", "author_email",
+                     "maintainer", "maintainer_email", "url",
+                     "license", "description", "long_description",
+                     "keywords", "platforms", "fullname", "contact",
+                     "contact_email")
+        for name in metanames:
+            method = "get_" + name
+            val = getattr(self.metadata, method)()
+            data.append("%s = %r" % (name, val))
+        # write the config file
+        util.execute(write_file, (filename, data),
+                     "creating %s" % filename, self.verbose >= 1, self.dry_run)
+
 
 setup(
-    name='wok',
-    version=version.encode("utf8"),
+    name=AppName,
+    version=AppVersion,
     author='Mike Cooper',
     author_email='mythmon@gmail.com',
     url='http://wok.mythmon.com',
@@ -18,13 +142,16 @@ setup(
         "directory of web files that you can upload to your web server, or "
         "serve directly.",
     download_url="http://wok.mythmon.com/download",
-    classifiers=[
+    distclass = MyDistribution,
+    cmdclass = {
+        'install_lib': MyInstallLib,
+    },
+    classifiers=(
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         'Operating System :: POSIX',
         'Programming Language :: Python',
-    ],
-    requires=['pyyaml', 'jinja2', 'Markdown', 'docutils', 'Pygments'],
+    ),
     packages=['woklib'],
     scripts=['wok'],
 )
