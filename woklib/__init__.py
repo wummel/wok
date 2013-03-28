4fb5929de7d318c051fc8233c03a42d0bed3ebb0
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-03-28 07:21:21 +0100

    Require python >= 2.7

--- woklib/__init__.py
+++ woklib/__init__.py
@@ -1,2 +1,8 @@
 # -*- coding: iso-8859-1 -*-
-# the wok library routines
+"""
+Library routines for the wok static homepage generator.
+"""
+import sys
+if not (hasattr(sys, 'version_info') or
+        sys.version_info < (2, 7, 0, 'final', 0)):
+    raise SystemExit("This program requires Python 2.7 or later.")
